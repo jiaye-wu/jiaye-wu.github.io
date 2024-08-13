7da186fc2524e504a55140a908d6f53734285c00
Jiaye Wu <161971145+jiaye-wu@users.noreply.github.com>
2024-08-13 14:35:14 +0200

    port Google Scholar crawler

---
+++ google_scholar_crawler/main.py
@@ -0,0 +1,39 @@
+from scholarly import scholarly
+import jsonpickle
+import json
+from datetime import datetime
+import os
+
+author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+name = author['name']
+author['updated'] = str(datetime.now())
+author['publications'] = {v['author_pub_id']:v for v in author['publications']}
+print(json.dumps(author, indent=2))
+os.makedirs('results', exist_ok=True)
+with open(f'results/gs_data.json', 'w') as outfile:
+    json.dump(author, outfile, ensure_ascii=False)
+
+shieldio_data = {
+  "schemaVersion": 1,
+  "label": "citations",
+  "message": f"{author['citedby']}",
+}
+with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
+    json.dump(shieldio_data, outfile, ensure_ascii=False)
+
+hindex_data = {
+  "schemaVersion": 1,
+  "label": "h-index",
+  "message": f"{author['hindex']}",
+}
+with open(f'results/gs_data_hindex.json', 'w') as outfile:
+    json.dump(hindex_data, outfile, ensure_ascii=False)
+
+i10_data = {
+  "schemaVersion": 1,
+  "label": "i10-index",
+  "message": f"{author['i10index']}",
+}
+with open(f'results/gs_data_i10.json', 'w') as outfile:
+    json.dump(i10_data, outfile, ensure_ascii=False)
