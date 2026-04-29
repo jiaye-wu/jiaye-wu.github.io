48ea763a40ee26924449c49f4f7ffd6d599040f6
jiaye-wu <161971145+jiaye-wu@users.noreply.github.com>
2026-04-29 12:11:45 +0000

    Deploying to gh-pages from @ jiaye-wu/jiaye-wu.github.io@2f4965fcfcb2ec42c284e84e9bab5a23f55148ba 🚀

--- Windows_local_prettier_plugin_install.cmd
+++
@@ -1,1 +0,0 @@
-npm install --save-dev @shopify/prettier-plugin-liquid
--- zh-hk/Windows_local_prettier_drag_to_write.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Formatting file(s) with Prettier...
-for %%F in (%*) do (
-    npx prettier --write "%%~F"
-)
-echo Done!
-pause
