1149f528ae5d4b722307d51d273d63457820e605
jiaye-wu <161971145+jiaye-wu@users.noreply.github.com>
2026-06-05 21:41:51 +0000

    Deploying to gh-pages from @ jiaye-wu/jiaye-wu.github.io@744be2b82e28a8bf021b418960b5cdf6b66b06a3 🚀

--- zh-cn/assets/js/search-data.js
+++ zh-cn/assets/js/search-data.js
@@ -1,7 +1,7 @@
 
 const currentUrl = window.location.href;
-const siteUrl = "https://jiaye-wu.github.io";
-let updatedUrl = currentUrl.replace("https://jiaye-wu.github.io", "");
+const siteUrl = "https://align.science";
+let updatedUrl = currentUrl.replace("https://align.science", "");
 if (currentUrl.length == updatedUrl.length && currentUrl.startsWith("http://127.0.0.1")) {
   const otherSiteUrl = siteUrl.replace("localhost", "127.0.0.1");
   updatedUrl = currentUrl.replace(otherSiteUrl + "", "");
